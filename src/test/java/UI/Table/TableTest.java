783ed770e5e4e67cfb9c0cc2dbbcb32a86902ffa
praveen <praveenbhananjaya@gmail.com>
2021-07-15 04:52:03 +0530

    table test

--- src/test/java/UI/Table/TableTest.java
+++ src/test/java/UI/Table/TableTest.java
@@ -1,19 +1,29 @@
 package UI.Table;
 
 import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TableTest {
 
-    @Test
-    void print() {
-        assertEquals(0,1);
-    }
 
     @Test
     void print1() {
 
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void testPrint1() {
+    }
+
+    @Test
+    void display() {
+    }
+
+    @Test
+    void main() {
     }
 }
