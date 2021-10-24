dd95c2a547b065bae7e0b3770c3889144e593f7f
praveen <praveenbhananjaya@gmail.com>
2021-10-24 14:30:36 +0530

    New pages Boot, HighHeels, LowHeels

--- core/tests.py
+++ core/tests.py
@@ -10,3 +10,24 @@ class URLTests(TestCase):
     def test_shopPage(self):
         response = self.client.get('/shop/')
         self.assertEqual(response.status_code, 200)
+
+    def test_BootPage(self):
+        response = self.client.get('/category/Boot')
+        # page redirection
+        self.assertEqual(response.status_code, 301)
+
+    def test_HighHeelsPage(self):
+        response = self.client.get('/category/heels')
+        # page redirection
+        self.assertEqual(response.status_code, 301)
+
+
+
+    def test_LowHeelsPage(self):
+        response = self.client.get('/category/lowheels/')
+        # page redirection
+        print(response.status_code)
+        self.assertEqual(response.status_code, 301)
+
+
+
