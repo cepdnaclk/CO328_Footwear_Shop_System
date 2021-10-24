bd436ec94cce0f58fc84743c12789e2665760485
praveen <praveenbhananjaya@gmail.com>
2021-10-24 14:06:07 +0530

    workflow update

--- core/tests.py
+++ core/tests.py
@@ -3,9 +3,10 @@ from django.test import TestCase
 
 # Create your tests here.
 class URLTests(TestCase):
-    def test_testhomepage(self):
+    def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_fail(self):
-        self.assertEqual(0, 0)
+    def test_shopPage(self):
+        response = self.client.get('/shop/')
+        self.assertEqual(response.status_code, 200)
